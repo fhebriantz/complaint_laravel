7a4c3aacc72fdfa3852dd02de5af3d24499a136c
fhebriantz <lutfi.febrianto@gmail.com>
2019-05-08 17:07:32 +0700

    Initial commit

---
+++ resources/views/include/left_menu.blade.php
@@ -0,0 +1,71 @@
+            <div id="left-menu">
+              <div class="sub-left-menu scroll">
+                <ul class="nav nav-list">
+                    <li><div class="left-bg"></div></li>
+                    <li class="time">
+                      <h1 class="animated fadeInLeft">21:00</h1>
+                      <p class="animated fadeInRight">Sat,October 1st 2029</p>
+                    
+                    <li class="ripple">
+                      <a href="{{url('/cms/slider')}}"> Slider 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li class="ripple">
+                      <a href="{{url('/cms/about')}}"> About 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li class="ripple">
+                      <a href="{{url('/cms/feature')}}"> Feature
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li class="ripple">
+                      <a href="{{url('/cms/portfolio')}}"> Portfolio 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li class="ripple">
+                      <a href="{{url('/cms/client')}}"> Client 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li class="ripple">
+                      <a href="{{url('/cms/team')}}"> Team 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li class="ripple">
+                      <a href="{{url('/cms/comment')}}"> Testimoni 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li class="ripple">
+                      <a href="{{url('/cms/blog')}}"> Blog 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li class="ripple">
+                      <a href="{{url('/cms/contact')}}"> Contact 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                    <li class="ripple">
+                      <a href="{{url('/cms/sosmed')}}"> Sosmed 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li>
+                     <li class="ripple">
+                      <a href="{{url('/cms/address')}}"> Address 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li> 
+                    <!-- <li class="ripple">
+                      <a href="{{url('/cms/career')}}"> Career 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                    </li> -->
+                  </ul>
+                </div>
+            </div>
